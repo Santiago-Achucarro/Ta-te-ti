87e96e02f097a811a5378ec225ab6eb9c43cab8c
Santiago Achucarro <achucarrosantiago02@gmail.com>
2023-04-11 12:57:15 -0300

    button reset and menu

--- src/components/ButtonSetts/index.jsx
+++ src/components/ButtonSetts/index.jsx
@@ -1,18 +1,31 @@
 import { Button } from "@mui/material";
+import { useContext } from "react";
+import { turnContext } from "../../custom/turnContext";
+import { useNavigate } from "react-router-dom";
 
-const ButtonSetts = ({ value, classes, background, hover }) => {
+const ButtonSetts = ({ value, background }) => {
+  const { handleReset } = useContext(turnContext);
+  const navigate = useNavigate();
+
+  const handleMenu = () => {
+    handleReset();
+    navigate("/");
+  };
+
   return (
     <Button
       sx={{
         backgroundColor: background,
         transition: "0.3s all ease-out",
+        boxShadow: "2px 2px 2px 0px black",
         color: value === "Restart" ? "white" : "black",
         "&:hover": {
           backgroundColor: background,
           transition: "0.3s all ease-in",
-          opacity:"0.8"
+          opacity: "0.6",
         },
       }}
+      onClick={() => (value === "Restart" ? handleReset() : handleMenu())}
     >
       {value}
     </Button>
